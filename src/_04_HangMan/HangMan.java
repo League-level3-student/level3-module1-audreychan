00447dd72d3c011b197e2d092d45dc7001105efe
League Student <league@iMac-18.attlocal.net>
2019-07-28 12:50:57 -0700

    HangMan, StringIntegerPair, IntroToHashMaps, LogSearch

--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -1,7 +1,9 @@
 package _04_HangMan;
 
+import java.awt.Dimension;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.ArrayList;
 import java.util.Stack;
 
 import javax.swing.JFrame;
@@ -14,8 +16,17 @@ public class HangMan implements KeyListener{
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
 	JLabel label = new JLabel();
+	JLabel winAndLose = new JLabel();
+	
+	int lives;
+	char typed;
+	int wins = 0;
+	int losses = 0;
+	String previousWord;
+	
 	Stack<String> words = new Stack<String>();
-	int lives = 6;
+	ArrayList<Character> letters = new ArrayList<Character>();
+	ArrayList<String> blanks = new ArrayList<String>();
 	
 	public static void main(String[] args) {
 		HangMan hangman = new HangMan();
@@ -39,22 +50,69 @@ public class HangMan implements KeyListener{
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
+		label.setPreferredSize(new Dimension(250, 30));
+		winAndLose.setPreferredSize(new Dimension(250, 30));
+		frame.setPreferredSize(new Dimension(300, 100));
+		
 		frame.add(panel);
 		panel.add(label);
+		panel.add(winAndLose);
 		frame.pack();
 		
 		frame.addKeyListener(this);
 		
-		newWord();
+		play();
 	}
 	
-	void newWord() {
-		String word = words.pop();
-		String blanks = "";
-		
-		for(int i = 0; i < word.length(); i++) {
-			blanks += "_";
+	void play() {
+		previousWord = "";
+		for(int i = 0; i < letters.size(); i++) {
+			previousWord += letters.get(i) + "";
 		}
+	
+		blanks.clear();
+		letters.clear();
+		
+		if(words.size() > 0) {
+			
+			String word = words.pop();
+			lives = 6;
+			
+			for(int i = 0; i < word.length(); i++) {
+				blanks.add("_");
+				letters.add(word.charAt(i));
+			}
+			
+			updateBlanks();
+		}
+	}
+	
+	void updateBlanks() {
+		String displayBlanks = "";
+		for(int i = 0; i < blanks.size(); i++) {
+			displayBlanks += blanks.get(i) + " ";
+		}
+		label.setText(displayBlanks + " | " + previousWord);
+		frame.pack();
+		updateWinAndLose();
+	}
+	
+	void updateWinAndLose() {
+		if(checkWinner() == true) {
+			wins++;
+			play();
+		}
+		winAndLose.setText("Wins: " + wins + "   Losses: " + losses + "   Lives: " + lives);
+		frame.pack();
+	}
+	
+	boolean checkWinner() {
+		for(int i = 0; i < blanks.size(); i++) {
+			if(blanks.get(i).equals("_")) {
+				return false;
+			}
+		}
+		return true;
 	}
 
 	@Override
@@ -64,7 +122,27 @@ public class HangMan implements KeyListener{
 
 	@Override
 	public void keyPressed(KeyEvent e) {
+		typed = e.getKeyChar();
+		boolean gotLetter = false;
 		
+		for(int i = 0; i < letters.size(); i++) {
+			if(letters.get(i) == typed) {
+				blanks.set(i, letters.get(i) + "");
+				gotLetter = true;
+			}
+		}
+		
+		updateBlanks();
+		
+		if(gotLetter == false) {
+			lives --;
+			updateWinAndLose();
+		}
+		if(lives == 0) {
+			losses ++;
+			updateWinAndLose();
+			play();
+		}
 	}
 
 	@Override
--- src/_06_Intro_To_Hash_Maps/_02_LogSearch.java
+++ src/_06_Intro_To_Hash_Maps/_02_LogSearch.java
@@ -1,8 +1,15 @@
 package _06_Intro_To_Hash_Maps;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.HashMap;
 
-public class _02_LogSearch {
-  /* 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+/* 
 	 * Crate a HashMap of Integers for the keys and Strings for the values.
 	 * Create a GUI with three buttons. 
 	 * Button 1: Add Entry
@@ -29,5 +36,82 @@ public class _02_LogSearch {
 	 * 				is not in the list. 
 	 *
 	 * */
+
+public class _02_LogSearch implements ActionListener {
+	
+	HashMap<Integer, String> log = new HashMap<Integer, String>();
+	
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JButton add = new JButton("Add Entry");
+	JButton remove = new JButton("Remove Entry");
+	JButton search = new JButton("Search by ID");
+	JButton	view = new JButton("View List");
+	
+	public static void main(String[] args) {
+		_02_LogSearch logSearch = new _02_LogSearch();
+		//logSearch.setup();
+	}
+	
+	public _02_LogSearch() {
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		add.addActionListener(this);
+		remove.addActionListener(this);
+		search.addActionListener(this);
+		view.addActionListener(this);
+		
+		frame.add(panel);
+		panel.add(add);
+		panel.add(remove);
+		panel.add(search);
+		panel.add(view);
+		
+		frame.pack();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		JButton pressed = (JButton) e.getSource();
+		if(pressed.equals(add)) {
+			String idString = JOptionPane.showInputDialog("Enter an ID.");
+			int id = Integer.parseInt(idString);
+			String name = JOptionPane.showInputDialog("Enter their name.");
+			log.put(id, name);
+		}
+		else if(pressed.equals(remove)) {
+			String idString = JOptionPane.showInputDialog("Enter an ID.");
+			int id = Integer.parseInt(idString);
+			String name = log.get(id);
+			if(name == null) {
+				JOptionPane.showMessageDialog(null, "Sorry, that user does not exist.");
+			}
+			else {
+				JOptionPane.showMessageDialog(null, name + " has been removed.");
+				log.remove(id);
+			}
+		}
+		else if(pressed.equals(search)) {
+			String idString = JOptionPane.showInputDialog("Enter an ID.");
+			int id = Integer.parseInt(idString);
+			String name = log.get(id);
+			if(name == null) {
+				JOptionPane.showMessageDialog(null, "Sorry, that user does not exist.");
+			} 
+			else {
+				JOptionPane.showMessageDialog(null, "ID #" + id + ": " + name);
+			}
+		}
+		else if(pressed.equals(view)) {
+			Object[] idList = log.keySet().toArray();
+			String list = "";
+			for(int i = 0; i < idList.length; i++) {
+				list += "ID #" + idList[i] + ": " + log.get(idList[i]) + "\n";
+			}
+			JOptionPane.showMessageDialog(null, list);
+		}
+	}
 	
 }
